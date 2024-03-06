bd2cbc84f3006a622c26bc262eb00d946e4ba8fb
Delawit12 <131721430+Delawit12@users.noreply.github.com>
2024-03-06 04:41:28 -0800

    Delete client directory

--- client/src/Api/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const axiosInstance = axios.create({
-  //     baseURL: "https://cute-ruby-cricket-gown.cyclic.cloud/"
-  //    // baseURL: "https://amazon-clone-g4tg.onrender.com"
-  baseURL: "http://localhost:7088/",
-});
-
-export default axiosInstance;
